9b77b3d2c90a14001591feedd0481c769942ba5e
leandroprada <leandroprada@gmail.com>
2019-07-04 15:07:40 -0300

    modifying login

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
  session_start();
  
- if (($_POST['username']) != "autorizado") {
+ if (($_POST['username']) !== "autorizado") {
 	
 	 echo "Su usuario NO ESTÁ AUTORIZADO";
 	 header("Location: ../portfolio.html");
