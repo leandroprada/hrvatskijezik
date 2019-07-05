a687fc1e9e95c4aca100ae5f756e16a348ffba55
leandro prada <leandroprada@gmail.com>
2019-07-05 19:47:41 -0300

    attemptingphp login

--- login/signup.php
+++ login/signup.php
@@ -1,12 +1,12 @@
 <?php
  session_start();
- 
+
 require 'dbconn.inc.php';
- 
+
  $username = $_POST['username'];
  $sql = "SELECT username FROM users WHERE username=?";
  $stmt = mysqli_stmt_init($conn);
- 
+
  if (!mysqli_stmt_prepare($stmt, $sql)){
 	  header("Location: login.php?error=wrongusername");
 	  echo "no estas conectado";
@@ -18,15 +18,15 @@ require 'dbconn.inc.php';
 		  mysqli_stmt_store_result($stmt);
 		  $resultCheck = mysqli_stmt_num_rows($stmt);
 		  if ($resultCheck >= 1) {
-			   header("Location: ../index.php?sucesslogin=true");
+			   header("Location: ../index1.php?sucesslogin=true");
 		  }
 		  else {
 			  header("Location: login.php?error=sqlcouldnotexecute");
 		  }
-		  
+
 	  }
-	
- 
+
+
 ?>
 
 <!DOCTYPE html>
@@ -35,7 +35,7 @@ require 'dbconn.inc.php';
 	<title>Login V14</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
@@ -45,13 +45,13 @@ require 'dbconn.inc.php';
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/util.css">
@@ -59,7 +59,7 @@ require 'dbconn.inc.php';
 <!--===============================================================================================-->
 </head>
 <body>
-	
+
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
@@ -75,7 +75,7 @@ require 'dbconn.inc.php';
 						<input class="input100" type="text" name="username" >
 						<span class="focus-input100"></span>
 					</div>
-					
+
 					<span class="txt1 p-b-11">
 						Password
 					</span>
@@ -86,7 +86,7 @@ require 'dbconn.inc.php';
 						<input class="input100" type="password" name="pass" >
 						<span class="focus-input100"></span>
 					</div>
-					
+
 					<div class="flex-sb-m w-full p-b-48">
 						<div class="contact100-form-checkbox">
 							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
@@ -112,10 +112,10 @@ require 'dbconn.inc.php';
 			</div>
 		</div>
 	</div>
-	
+
 
 	<div id="dropDownSelect1"></div>
-	
+
 <!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
