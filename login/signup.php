17390e55e2b58c8030556ac1b9f1295812c75a36
leandroprada <leandroprada@gmail.com>
2019-07-05 18:04:04 -0300

    attempt at login w given user

--- login/signup.php
+++ login/signup.php
@@ -16,7 +16,7 @@ require 'dbconn.inc.php';
  $stmt = mysqli_stmt_init($conn);
  
  if (!mysqli_stmt_prepare($stmt, $sql)){
-	  header("Location: ../../lap/index.html");
+	  header("Location: login.php");
 	  echo "no estas conectado";
 	  exit();
 	  }
@@ -26,7 +26,7 @@ require 'dbconn.inc.php';
 		  mysqli_stmt_store_result($stmt);
 		  $resultCheck = mysqli_stmt_num_rows($stmt);
 		  if ($resultCheck = 1) {
-			   header("Location: ../../lap/index.html");
+			   header("Location: index.html");
 		  }
 		  else {
 			  // header("Location: ../login/login.php");
