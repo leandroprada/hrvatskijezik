f580bc5cb1643ca721a827847f698557ca5d9be3
leandroprada <leandroprada@gmail.com>
2019-07-05 17:34:22 -0300

    attempt at login w given user

--- login/login.php
+++ login/login.php
@@ -1,29 +1,7 @@
 <?php
  session_start();
 $_SESSION['username'] = $_POST['username']; 
- require './includes/dbconn.inc.php';
  
- $username = $_POST['username'];
- $sql = "SELECT username FROM users WHERE username=?";
- $stmt = mysqli_stmt_init($conn);
- 
- if (!mysqli_stmt_prepare($stmt, $sql)){
-	  header("Location: ../login/login.php");
-	  exit();
-	  }
-	  else {
-		  mysqli_stmt_bind_param ($stmt, "s", $username);
-		  mysqli_stmt_execute($stmt);
-		  mysqli_stmt_store_result($stmt);
-		  $resultCheck = mysqli_stmt_num_rows($stmt);
-		  if ($resultCheck = 1) {
-			  header("Location: ../index.php");
-		  }
-		  else {
-			  header("Location: ../login/login.php");
-		  }
-		  
-	  }
  
 ?>
 
