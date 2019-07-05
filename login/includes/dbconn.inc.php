6c7cc0761638469c4d07b95d82d5ea225d3d9145
leandroprada <leandroprada@gmail.com>
2019-07-05 17:29:26 -0300

    attempt at login w given user

--- login/includes/dbconn.inc.php
+++ login/includes/dbconn.inc.php
@@ -5,7 +5,7 @@ $dbUsername = "root";
 $dbPassword = "Malonasima01";
 $dbName = "jezik";
 
-$conn = mysqli_connect ($servername,$dbUsername,$dbPassword,$dbName);
+$conn = mysqli_connect($servername,$dbUsername,$dbPassword,$dbName);
 
 if (!conn){
     die("connection failed: ".mysqli_connect_error());
