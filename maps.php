26ee1cd7f28d865e85c96dccaa8395c9f696ad8b
leandro prada <leandroprada@gmail.com>
2019-07-08 19:25:35 -0300

    mapped cara

--- maps.php
+++ maps.php
@@ -191,8 +191,17 @@
 								<h3 class="center">Cara</h3>
 								<img class="img-fluid map" src="img/maps/cara.jpg" alt="" usemap="#image-mapcr">
 								<map name="image-mapcr">
-								<area target="" alt="departamento - stan" title="departamento - stan" href="" coords="103,120,319,117,334,184,90,188" shape="poly">
-								<area target="" alt="edificio - zgrada" title="edificio - zgrada" href="" coords="0,637,426,638,425,565,315,100,105,98" shape="poly">
+								<area target="" alt="pelo - kosa" title="pelo - kosa" href="" coords="31,581,114,510,190,458,208,387,202,297,218,187,261,116,308,101,360,108,409,134,436,182,455,204,500,210,494,256,466,301,432,386,398,426,565,441,536,240,528,182,504,114,472,35,423,2,233,2,178,50,142,87,139,126,97,188,67,253,58,319,45,355,25,482,2,554" shape="poly">
+								<area target="" alt="labio - usna" title="labio - usna" href="" coords="261,348,290,384,308,390,330,390,342,388,362,375,391,344,366,361,342,370,309,373,292,364,274,358" shape="poly">
+								<area target="" alt="boca - usta" title="boca - usta" href="" coords="260,342,318,349,360,339,391,331,364,378,342,388,316,391,293,384,276,370" shape="poly">
+								<area target="" alt="nariz . nos" title="nariz . nos" href="" coords="332,228,314,225,302,225,291,235,297,272,299,298,286,299,286,321,302,330,316,336,327,332,338,323,351,314,355,298,345,290,336,294,336,264" shape="poly">
+								<area target="" alt="ojo - oko" title="ojo - oko" href="" coords="341,235,358,243,377,245,395,241,413,230,406,217,386,212,367,213,348,220" shape="poly">
+								<area target="" alt="mejilla - obraz" title="mejilla - obraz" href="" coords="345,249,348,282,390,326,395,364,403,398,433,367,450,330,453,293,437,262,420,251,390,255" shape="poly">
+								<area target="" alt="oreja - oreja" title="oreja - oreja" href="" coords="460,215,466,237,466,261,464,287,475,287,489,265,495,246,495,225,495,213,468,207" shape="poly">
+								<area target="" alt="frente - celo" title="frente - celo" href="" coords="218,212,272,208,306,214,342,200,395,195,424,203,440,192,405,136,361,112,309,104,264,119,228,172" shape="poly">
+								<area target="" alt="pera - brada" title="pera - brada" href="" coords="272,392,303,402,340,402,359,395,382,385,386,412,363,429,330,436,299,436,280,423" shape="poly">
+								<area target="" alt="ceja - obrva" title="ceja - obrva" href="" coords="285,225,275,213,246,213,223,215,214,220,210,233,230,223,255,220,270,225" shape="poly">
+								<area target="" alt="pestañas - trepavice" title="pestañas - trepavice" href="" coords="283,240,266,233,253,235,240,239,230,248,218,244,230,234,242,228,257,226,278,230" shape="poly">
 								</map>
 							</div>
 						</div>
