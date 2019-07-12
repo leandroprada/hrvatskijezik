33107c92756d01495bbc02c25b416e0886f41b73
leandroprada <leandroprada@gmail.com>
2019-07-12 16:11:05 -0300

    trying to add header to login and pwdHasher

--- login/login.php
+++ login/login.php
@@ -8,11 +8,11 @@ $_SESSION['username'] = $_POST['username'];
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login V14</title>
+	<title>Ingreso - La Lengua Croata</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
