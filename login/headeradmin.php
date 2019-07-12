45579c98f83a1c495d4658db8635c65609b9b195
leandroprada <leandroprada@gmail.com>
2019-07-12 16:31:29 -0300

    trying to add header to login and pwdHasher

--- login/headeradmin.php
+++ login/headeradmin.php
@@ -5,7 +5,7 @@
 
   <header id="header">
 		    <div class="container main-menu">
-		    	<div class="row align-items-center justify-content-between d-flex">
+		    	<div class="row align-items-center justify-content-between d-flex" style="background-color:transparent;">
 			      <div id="logo">
 			        <a href="../index.php"><img src="../img/logo.png" alt="" title="" /></a>
 			      </div>
